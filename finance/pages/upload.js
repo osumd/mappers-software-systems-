0fed321b87c60773bfa2ddfb4426ab34739b86b5
ajh416 <henryada@oregonstate.edu>
2023-10-31 15:15:06 -0700

    clear page on file upload

--- finance/pages/upload.js
+++ finance/pages/upload.js
@@ -6,29 +6,29 @@ function OnFileSubmit() {
     const files = input.files
     if (files.length > 0) {
         console.log('file uploaded')
+        let main = document.querySelector('main')
+        while (main.hasChildNodes()) {
+            main.removeChild(main.firstChild)
+        }
+        let h2 = document.createElement('h2')
+        h2.textContent = "Financial Information Processing"
+        main.appendChild(h2)
     }
-    let main = document.querySelector('main')
-    while (main.hasChildNodes()) {
-        main.removeChild(main.firstChild)
-    }
-    let h2 = document.createElement('h2')
-    h2.textContent = "Financial Information Processing"
-    main.appendChild(h2)
 }
 
 export default function Home() {
     return (
         <>
-        <Head>
-            <title>Upload Financial Information</title>
-        </Head>
-        <main className={styles.main}>
-            <h2>Upload Financial Information</h2>
+            <Head>
+                <title>Upload Financial Information</title>
+            </Head>
+            <main className={styles.main}>
+                <h2>Upload Financial Information</h2>
                 <div className={styles.form_container}>
                     <input type='file'></input>
                     <button onClick={OnFileSubmit}>Upload Financial CSV</button>
                 </div>
-        </main>
+            </main>
         </>
     )
 }

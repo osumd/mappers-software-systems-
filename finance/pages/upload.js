6421506d3141d3c8787bdda19e181b09fbabfe57
ajh416 <henryada@oregonstate.edu>
2023-11-07 14:55:26 -0800

    remove old function

--- finance/pages/upload.js
+++ finance/pages/upload.js
@@ -2,30 +2,6 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { useEffect } from 'react'
 
-// function OnFileSubmit() {
-//     const form = document.querySelector('form')
-//     form.addEventListener('submit', handleSubmit)
-
-//     let input = document.querySelector('input')
-//     const files = input.files
-//     if (files.length < 1) {
-//         return
-//     }
-
-//     console.log('file uploaded')
-//     let main = document.querySelector('main')
-
-//     // clear the page
-//     while (main.hasChildNodes()) {
-//         main.removeChild(main.firstChild)
-//     }
-
-//     // create the header and tell the user we are processing
-//     let h2 = document.createElement('h2')
-//     h2.textContent = 'Financial Information Processing'
-//     main.appendChild(h2)
-// }
-
 export default function Home() {
     useEffect(() => {
         const form = document.querySelector('form')

e30631a2bb8fe44cc53667caf88ad355649acd61
ajh416 <henryada@oregonstate.edu>
2023-11-08 12:41:34 -0800

    fix server.js to send response to upload, add try/catch to upload.js

--- backend/server.js
+++ backend/server.js
@@ -20,20 +20,31 @@ app.listen(port, () => {
 });
 
 app.post('/api/upload', (req, res, next) => {
-    const form = new formidable.IncomingForm();
+    const form = new formidable.IncomingForm()
 
+    form.maxFileSize = 50*1024*1024 // 5MB
+
+    // calling parse() will parse the form and extract the fields and files into objects similar to arrays
     form.parse(req, (err, fields, files) => {
         if (err) {
             next(err);
             return;
         }
-        if (files[0] === undefined) {
+
+        // the variable is called files, and the name of the form in the html is also files, so we get this abomination of 'files.files'
+        console.log(files.files)
+
+        // if we did not receive files, assume it's just text
+        if (files.files === undefined) {
             ParseManualTransaction(fields)
+        } else {
+            
         }
-        res.status(200);
+
+        res.status(200).send();
     });
 })
 
 function ParseManualTransaction(fields) {
-    console.log(fields) // example: { title: [ 'title' ], money: [ 'money' ], category: [ '' ] }
+    console.log({ fields }) // example: { title: [ 'title' ], money: [ 'money' ], category: [ '' ] }
 }
--- finance/pages/upload.js
+++ finance/pages/upload.js
@@ -1,55 +1,79 @@
 import Head from 'next/head'
-import {useEffect} from 'react'
+import { useEffect } from 'react'
 
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  function handleFileSubmit(event) {
-    const form = event.currentTarget
-    const url = new URL(form.action);
-    const formData = new FormData(form);
-    const fetchOptions = {
-      method: form.method,
-      body: formData,
-    };
+    async function handleFileSubmit(event) {
+        // prevents the form from accessing '/api' unless fetch fails
+        event.preventDefault()
 
-    if (formData.get('title') != '') {
-      fetch(url, fetchOptions)
-      alert('File submitted')
-      return
-    }
-    alert('File must have title')
+        const form = event.currentTarget
+        const url = new URL(form.action);
+        const formData = new FormData(form);
+        const fetchOptions = {
+            mode: 'cors',
+            method: form.method,
+            body: formData,
+        };
 
-    // prevents the form from accessing '/api' unless fetch fails
-    event.preventDefault()
-  }
+        if (formData.get('title') != '' && formData.get('files').name != '') {
+            try {
+                const response = await fetch(url, fetchOptions)
 
-  function handleManualSubmit(event) {
-    const form = event.currentTarget
-    const url = new URL(form.action)
-    const formData = new FormData(form)
-    const fetchOptions = {
-        method: form.method,
-        body: formData,
-    }
+                const result = response.status
+                
+                if (result == 200) {
+                    alert('File submitted')
+                }
+                else {
+                    alert('File failed to submit')
+                }
 
-    if (formData.get('title') != '' && formData.get('money') != '') {
-        fetch(url, fetchOptions)
-        alert('Transaction submitted')
-    }
-    else {
-        alert("Title and monetary value may not be empty")
+            } catch (e) { console.log(e) }
+        } else {
+            alert('File must have title')
+        }
     }
 
-    event.preventDefault()
-  }
+    async function handleManualSubmit(event) {
+        event.preventDefault()
+
+        const form = event.currentTarget
+        const url = new URL(form.action)
+        const formData = new FormData(form)
+        const fetchOptions = {
+            mode: 'cors',
+            method: form.method,
+            body: formData,
+        }
+
+        if (formData.get('title') != '' && formData.get('money') != '') {
+            try {
+                const response = await fetch(url, fetchOptions)
+
+                const result = response.status
+                
+                if (result == 200) {
+                    alert('Transaction submitted')
+                }
+                else {
+                    alert('Transaction failed to submit, please try again later')
+                }
+
+            } catch (e) { console.log(e) }
+        }
+        else {
+            alert("Title and monetary value may not be empty")
+        }
+    }
 
     useEffect(() => {
         const form = document.querySelector('form')
         form.addEventListener('submit', handleFileSubmit)
 
         const manualForm = document.querySelector('.manualForm')
-        manualForm.addEventListener('submit', handleManualSubmit)
+        manualForm.addEventListener('submit', async (event) => { await handleManualSubmit(event) })
     })
     return (
         <>
@@ -69,17 +93,17 @@ export default function Home() {
                     <div className={styles.upload_flex_child}>
                         <h3>Type in Transaction</h3>
                         <form className="manualForm" action='http://localhost:5000/api/upload' method='post' encType='multipart/form-data'>
-                        <table>
-                            <tbody>
-                                <tr><th>Title/Name</th><th>Monetary Value</th><th>Category</th></tr>
-                                <tr>
-                                    <td><input type='text' name='title'/></td>
-                                    <td><input type="text" name="money"/></td>
-                                    <td><input type="text" name="category"/></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <button>Submit Transaction</button>
+                            <table>
+                                <tbody>
+                                    <tr><th>Title/Name</th><th>Monetary Value</th><th>Category</th></tr>
+                                    <tr>
+                                        <td><input type='text' name='title' /></td>
+                                        <td><input type="text" name="money" /></td>
+                                        <td><input type="text" name="category" /></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <button>Submit Transaction</button>
                         </form>
                     </div>
                 </div>

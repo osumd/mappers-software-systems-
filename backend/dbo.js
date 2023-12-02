e1be25d8ed0d084a99f565a229a53a302b6ecdc3
blacksea <quetran.tran1009@gmail.com>
2023-12-02 11:33:13 -0800

    change

--- backend/dbo.js
+++ backend/dbo.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://trantr3:tristanwr122@trantr3.zfqmrml.mongodb.net/"
+const uri = "mongodb+srv://mappers123:mappers123@cluster0.kmywt7n.mongodb.net/?retryWrites=true&w=majority";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
